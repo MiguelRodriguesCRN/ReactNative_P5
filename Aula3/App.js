1f85d256ec0b4d3f62afd0d6bc6a6ed2937f017e
MiguelRodriguesCRN <miguelrodriguesdev12@gmail.com>
2025-10-09 05:35:28 -0300

    Atividades

---
+++ Aula3/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import LoginsScreen from "./src/screens/LoginsScreen";
+
+const App = () => {
+    return (
+
+        <LoginsScreen />
+
+    );
+};
+
+export default App;
